6a62666ac793b35af1264b2f799c3d7802153eb6
Ranjit Chaudhary <107805675+Ranjitdon@users.noreply.github.com>
2025-06-14 10:11:59 +0545

    Update vite.config.ts

--- playful-learning-universe/vite.config.ts
+++ playful-learning-universe/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,9 +10,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
+  base: process.env.VITE_BASE_PATH || "/AI_Based_Learning_Platform/playful-learning-universe",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

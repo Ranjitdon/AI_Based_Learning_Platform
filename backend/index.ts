a1d0155b659e12a9a3392a8ed7cc91c019e11f00
Ranjit Chaudhary <107805675+Ranjitdon@users.noreply.github.com>
2025-06-15 15:11:44 +0545

    Update index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -12,7 +12,10 @@ mongodbConnect();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: "https://ai-based-learning-platform-r7k4.vercel.app",
+  credentials: true, // if you are using cookies/auth
+}));
 
 app.options("*", cors()); // handle preflight
 
